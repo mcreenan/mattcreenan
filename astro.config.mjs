04b4b440a3d01301e2786e8e9fc249ff57761630
Matt Creenan <mattcreenan@gmail.com>
2024-11-01 22:21:52 -0400

    Style tweak; update site url for preview cards

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://matt.creenan.me',
     integrations: [mdx(), sitemap(), tailwind()]
 });
